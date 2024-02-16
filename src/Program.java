de968b4df8d86b2f57367f3759ad7b8b566f3a17
Caio Lucena <caio.lucena10@hotmail.com>
2024-02-16 19:44:01 -0300

    saving some treatments for comma

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-//TODO tratar casos semelhantes a 2.b, 3.c (vírgulas também devem ser retiradas do logradouro) e 3.d (há um " a mais no campo número)
+//TODO tratar caso 2.b (padrão é de apenas uma letra depois do número, vide caso 1.c)
+//TODO pensar em mais casos de erros
 
 public class Program {
     public static void main(String[] args) {
@@ -12,52 +13,51 @@ public class Program {
             String fullAddress = scanner.nextLine();
 
             String [] vector = fullAddress.split(" ");
+            String [] auxVector = new String[vector.length];
 
             String address = "";
             String addressNumber = "";
-            String addressNumberWithoutComma = "";
             String separator = " ";
 
             int aux = 0;
 
             for (int i = 0; i < vector.length; i++){
                 for (int j = 0; j < vector[i].length(); j++){
-                    if(Character.isDigit(vector[i].charAt(j))){
-                        addressNumber = vector[i];
+
+                    auxVector[i] = vector[i].replace(",", "");
+
+                }
+            }
+
+            for (int i = 0; i < auxVector.length; i++){
+                for (int j = 0; j < auxVector[i].length(); j++){
+                    if(Character.isDigit(auxVector[i].charAt(j))){
+                        addressNumber = auxVector[i];
                         aux = i;
 
                     }
                 }
             }
 
-            boolean isAbbreviation = aux > 0 && (vector[aux -1].equalsIgnoreCase("n°") || vector[aux -1].equals("No"));
-            boolean containsComma = vector[aux].contains(",");
+            boolean isAbbreviation = aux > 0 && (auxVector[aux -1].equals("No"));
+
 
             if (isAbbreviation){
                 String auxAddressNumber = addressNumber;
-                addressNumber = vector[aux-1].concat(separator).concat(auxAddressNumber);
-                vector[aux-1] = "";
+                addressNumber = auxVector[aux-1].concat(separator).concat(auxAddressNumber);
+                auxVector[aux-1] = "";
             }
 
-            if(containsComma){
-                addressNumberWithoutComma = addressNumber.replace(",", "");
-            }
-
-
-            for (int i = 0; i < vector.length; i++){
+            for (int i = 0; i < auxVector.length; i++){
                 if(i != aux){
-                    String auxAddress = vector[i];
-
-                    address = address.concat(auxAddress).concat(" ");
+                    String auxAddress = auxVector[i];
+                    address = address.concat(auxAddress).concat(separator);
                 }
             }
 
-            if(containsComma){
-                System.out.println("{\"" + address.trim() + "\"" + ", \"" + addressNumberWithoutComma.trim() + "\"}");
-            }
-            else {
-                System.out.println("{\"" + address.trim() + "\"" + ", \"" + addressNumber.trim() + "\"}");
-            }
+            String addressNumberWithoutComma = addressNumber.replace(",", "");
+
+            System.out.println("{\"" + address.trim() + "\"" + ", \"" + addressNumberWithoutComma.trim() + "\"}");
 
         }
         catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e){
