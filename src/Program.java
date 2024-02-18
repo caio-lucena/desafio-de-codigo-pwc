fc118d58caeca3e800ffb1d1dc6760574d42f8be
Caio Lucena <caio.lucena10@hotmail.com>
2024-02-17 22:38:47 -0300

    extracting the logic to a method

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-//TODO tratar caso 2.b (padrão é de apenas uma letra depois do número, vide caso 1.c)
 //TODO pensar em mais casos de erros
 
 public class Program {
@@ -12,57 +11,7 @@ public class Program {
             System.out.print("Informe o seu endereço: ");
             String fullAddress = scanner.nextLine();
 
-            String [] vector = fullAddress.split(" ");
-            String [] auxVector = new String[vector.length];
-
-            String address = "";
-            String addressNumber = "";
-            String separator = " ";
-
-            int aux = 0;
-
-            for (int i = 0; i < vector.length; i++){
-                for (int j = 0; j < vector[i].length(); j++){
-
-                    auxVector[i] = vector[i].replace(",", "");
-
-                }
-            }
-
-            for (int i = 0; i < auxVector.length; i++){
-                for (int j = 0; j < auxVector[i].length(); j++){
-                    if(Character.isDigit(auxVector[i].charAt(j))){
-                        addressNumber = auxVector[i];
-                        aux = i;
-
-                    }
-                }
-            }
-
-            boolean isAbbreviation = aux > 0 && (auxVector[aux -1].equals("No"));
-            boolean isAddressComplement = (aux + 1 < auxVector.length) && (auxVector[aux + 1].length() == 1);
-
-            if (isAbbreviation){
-                String auxAddressNumber = addressNumber;
-                addressNumber = auxVector[aux-1].concat(separator).concat(auxAddressNumber);
-                auxVector[aux-1] = "";
-            }
-
-            if (isAddressComplement){
-                addressNumber = addressNumber.concat(separator).concat(vector[aux + 1]);
-                auxVector[aux + 1] = "";
-            }
-
-            for (int i = 0; i < auxVector.length; i++){
-                if(i != aux){
-                    String auxAddress = auxVector[i];
-                    address = address.concat(auxAddress).concat(separator);
-                }
-            }
-
-            String addressNumberWithoutComma = addressNumber.replace(",", "");
-
-            System.out.println("{\"" + address.trim() + "\"" + ", \"" + addressNumberWithoutComma.trim() + "\"}");
+            System.out.println(formatAddress(fullAddress));
 
         }
         catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e){
@@ -71,5 +20,61 @@ public class Program {
         finally {
             scanner.close();
         }
+    }
+
+    public static String formatAddress(String fullAddress){
+
+        String [] vector = fullAddress.split(" ");
+        String [] auxVector = new String[vector.length];
+
+        String address = "";
+        String addressNumber = "";
+        String separator = " ";
+
+        int aux = 0;
+
+        for (int i = 0; i < vector.length; i++){
+            for (int j = 0; j < vector[i].length(); j++){
+
+                auxVector[i] = vector[i].replace(",", "");
+
+            }
+        }
+
+        for (int i = 0; i < auxVector.length; i++){
+            for (int j = 0; j < auxVector[i].length(); j++){
+                if(Character.isDigit(auxVector[i].charAt(j))){
+                    addressNumber = auxVector[i];
+                    aux = i;
+
+                }
+            }
+        }
+
+        boolean isAbbreviation = aux > 0 && (auxVector[aux -1].equals("No"));
+        boolean isAddressComplement = (aux + 1 < auxVector.length) && (auxVector[aux + 1].length() == 1);
+
+        if (isAbbreviation){
+            String auxAddressNumber = addressNumber;
+            addressNumber = auxVector[aux-1].concat(separator).concat(auxAddressNumber);
+            auxVector[aux-1] = "";
+        }
+
+        if (isAddressComplement){
+            addressNumber = addressNumber.concat(separator).concat(vector[aux + 1]);
+            auxVector[aux + 1] = "";
+        }
+
+        for (int i = 0; i < auxVector.length; i++){
+            if(i != aux){
+                String auxAddress = auxVector[i];
+                address = address.concat(auxAddress).concat(separator);
+            }
+        }
+
+        String addressNumberWithoutComma = addressNumber.replace(",", "");
+
+        return "{\"" + address.trim() + "\"" + ", \"" + addressNumberWithoutComma.trim() + "\"}";
+
     }
 }
