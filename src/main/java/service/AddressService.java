5f2f772115ed6df826c1ba923155a967d2b228b4
caio-lucena <caio.lucena10@hotmail.com>
2024-02-21 00:43:22 -0300

    corrigindo erro na classe AddressService

--- src/main/java/service/AddressService.java
+++ src/main/java/service/AddressService.java
@@ -5,20 +5,19 @@ import generated.FormatAddressGrpc;
 import generated.addressFmt;
 import io.grpc.stub.StreamObserver;
 
+
 public class AddressService extends FormatAddressGrpc.FormatAddressImplBase {
     @Override
     public void request(addressFmt request, StreamObserver<APIResponse> responseObserver) {
        String fullAddress = request.getAddress();
 
+
        APIResponse.Builder response = APIResponse.newBuilder();
 
-       if(!fullAddress.isEmpty()){
-           response.setResponseMessage(AddressFormatter.format(fullAddress));
-           response.setResponseCode(0);
-       }
-       else {
-          response.setResponseMessage("Endereço não informado");
-          response.setResponseCode(3);
-       }
+       response.setResponseMessage(AddressFormatter.format(fullAddress)).setResponseCode(200);
+
+       responseObserver.onNext(response.build());
+       responseObserver.onCompleted();
+
     }
 }
